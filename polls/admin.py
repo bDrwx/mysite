5e72ab9b3aa1865241b1ad0be6276b44bc1e6e25
bDrwx <a.bodosov@gmail.com>
2022-03-20 21:01:33 +0300

    Less 2 final

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Qustion
+
+admin.site.register(Qustion)
--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,24 @@
+from datetime import timedelta
 from secrets import choice
 from django.db import models
+from django.utils import timezone
 
 
 class Qustion(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField("date published")
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self) -> str:
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Qustion, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
